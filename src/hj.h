15c8b32c04ecbb4c55bab7d1698296b62ff40a2d
Hussam al-Homsi <sawuare@gmail.com>
2022-02-13 04:08:32 -0500

    119

--- src/hj.h
+++ src/hj.h
@@ -20,15 +20,15 @@
 
 #define HJ_IS_MONO_XOR_RAMP(id) ((id) < 40)
 
-#define PAINTER(id) static inline void hj_painter##id(void)
-
-#define FOR_YX \
-  uint32_t yn = hj_y0 + hj_height; \
-  uint32_t xn = hj_x0 + hj_width; \
-  for (uint32_t y = hj_y0; y < yn; ++y) \
-  for (uint32_t x = hj_x0; x < xn; ++x)
-
-#define CURRENT_PX hj_canvas[(y - hj_y0) * hj_width + x - hj_x0]
+#define PAINTER(id, formula)                                     \
+  static inline void hj_painter##id(void) {                      \
+    uint32_t yn = hj_y0 + hj_height;                             \
+    uint32_t xn = hj_x0 + hj_width;                              \
+                                                                 \
+    for (uint32_t y = hj_y0; y < yn; ++y)                        \
+      for (uint32_t x = hj_x0; x < xn; ++x)                      \
+        hj_canvas[(y - hj_y0) * hj_width + x - hj_x0] = formula; \
+  }
 
 #define MONO(value) (value) ? HJ_BLACK : hj_color
 #define RAMP(value) (value) % 256 * hj_color / 255
@@ -77,247 +77,67 @@ static inline _Bool hj_is_defined(void) {
 }
 
 // Class 0
-
-PAINTER(0) {
-  FOR_YX
-    CURRENT_PX = MONO((x & y) & J);
-}
-
-PAINTER(1) {
-  FOR_YX
-    CURRENT_PX = MONO((x | y) & J);
-}
-
-PAINTER(2) {
-  FOR_YX
-    CURRENT_PX = MONO((x ^ y) & J);
-}
-
-PAINTER(3) {
-  FOR_YX
-    CURRENT_PX = MONO((x + y) & J);
-}
-
-PAINTER(4) {
-  FOR_YX
-    CURRENT_PX = MONO((x * y) & J);
-}
+PAINTER(0, MONO((x & y) & J))
+PAINTER(1, MONO((x | y) & J))
+PAINTER(2, MONO((x ^ y) & J))
+PAINTER(3, MONO((x + y) & J))
+PAINTER(4, MONO((x * y) & J))
 
 // Class 1
-
-PAINTER(5) {
-  FOR_YX
-    CURRENT_PX = MONO((x & y) % J);
-}
-
-PAINTER(6) {
-  FOR_YX
-    CURRENT_PX = MONO((x | y) % J);
-}
-
-PAINTER(7) {
-  FOR_YX
-    CURRENT_PX = MONO((x ^ y) % J);
-}
-
-PAINTER(8) {
-  FOR_YX
-    CURRENT_PX = MONO((x + y) % J);
-}
-
-PAINTER(9) {
-  FOR_YX
-    CURRENT_PX = MONO((x * y) % J);
-}
+PAINTER(5, MONO((x & y) % J))
+PAINTER(6, MONO((x | y) % J))
+PAINTER(7, MONO((x ^ y) % J))
+PAINTER(8, MONO((x + y) % J))
+PAINTER(9, MONO((x * y) % J))
 
 // Class 2
-
-PAINTER(10) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x & y * y) & J);
-}
-
-PAINTER(11) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x | y * y) & J);
-}
-
-PAINTER(12) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x ^ y * y) & J);
-}
-
-PAINTER(13) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x + y * y) & J);
-}
-
-PAINTER(14) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x * y * y) & J);
-}
+PAINTER(10, MONO((x * x & y * y) & J))
+PAINTER(11, MONO((x * x | y * y) & J))
+PAINTER(12, MONO((x * x ^ y * y) & J))
+PAINTER(13, MONO((x * x + y * y) & J))
+PAINTER(14, MONO((x * x * y * y) & J))
 
 // Class 3
-
-PAINTER(15) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x & y * y) % J);
-}
-
-PAINTER(16) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x | y * y) % J);
-}
-
-PAINTER(17) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x ^ y * y) % J);
-}
-
-PAINTER(18) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x + y * y) % J);
-}
-
-PAINTER(19) {
-  FOR_YX
-    CURRENT_PX = MONO((x * x * y * y) % J);
-}
+PAINTER(15, MONO((x * x & y * y) % J))
+PAINTER(16, MONO((x * x | y * y) % J))
+PAINTER(17, MONO((x * x ^ y * y) % J))
+PAINTER(18, MONO((x * x + y * y) % J))
+PAINTER(19, MONO((x * x * y * y) % J))
 
 // Class 4
-
-PAINTER(20) {
-  FOR_YX
-    CURRENT_PX = MONO(((J & x) * x & (J & y) * y) & J);
-}
-
-PAINTER(21) {
-  FOR_YX
-    CURRENT_PX = MONO(((J | x) * x & (J | y) * y) & J);
-}
-
-PAINTER(22) {
-  FOR_YX
-    CURRENT_PX = MONO(((J ^ x) * x & (J ^ y) * y) & J);
-}
-
-PAINTER(23) {
-  FOR_YX
-    CURRENT_PX = MONO(((J + x) * x & (J + y) * y) & J);
-}
-
-PAINTER(24) {
-  FOR_YX
-    CURRENT_PX = MONO(((J * x) * x & (J * y) * y) & J);
-}
+PAINTER(20, MONO(((J & x) * x & (J & y) * y) & J))
+PAINTER(21, MONO(((J | x) * x & (J | y) * y) & J))
+PAINTER(22, MONO(((J ^ x) * x & (J ^ y) * y) & J))
+PAINTER(23, MONO(((J + x) * x & (J + y) * y) & J))
+PAINTER(24, MONO(((J * x) * x & (J * y) * y) & J))
 
 // Class 5
-
-PAINTER(25) {
-  FOR_YX
-    CURRENT_PX = MONO(((J & x) * x & (J & y) * y) % J);
-}
-
-PAINTER(26) {
-  FOR_YX
-    CURRENT_PX = MONO(((J | x) * x & (J | y) * y) % J);
-}
-
-PAINTER(27) {
-  FOR_YX
-    CURRENT_PX = MONO(((J ^ x) * x & (J ^ y) * y) % J);
-}
-
-PAINTER(28) {
-  FOR_YX
-    CURRENT_PX = MONO(((J + x) * x & (J + y) * y) % J);
-}
-
-PAINTER(29) {
-  FOR_YX
-    CURRENT_PX = MONO(((J * x) * x & (J * y) * y) % J);
-}
+PAINTER(25, MONO(((J & x) * x & (J & y) * y) % J))
+PAINTER(26, MONO(((J | x) * x & (J | y) * y) % J))
+PAINTER(27, MONO(((J ^ x) * x & (J ^ y) * y) % J))
+PAINTER(28, MONO(((J + x) * x & (J + y) * y) % J))
+PAINTER(29, MONO(((J * x) * x & (J * y) * y) % J))
 
 // Class 6
-
-PAINTER(30) {
-  FOR_YX
-    CURRENT_PX = MONO(((J & x) * x + (J & y) * y) & J);
-}
-
-PAINTER(31) {
-  FOR_YX
-    CURRENT_PX = MONO(((J | x) * x + (J | y) * y) & J);
-}
-
-PAINTER(32) {
-  FOR_YX
-    CURRENT_PX = MONO(((J ^ x) * x + (J ^ y) * y) & J);
-}
-
-PAINTER(33) {
-  FOR_YX
-    CURRENT_PX = MONO(((J + x) * x + (J + y) * y) & J);
-}
-
-PAINTER(34) {
-  FOR_YX
-    CURRENT_PX = MONO(((J * x) * x + (J * y) * y) & J);
-}
+PAINTER(30, MONO(((J & x) * x + (J & y) * y) & J))
+PAINTER(31, MONO(((J | x) * x + (J | y) * y) & J))
+PAINTER(32, MONO(((J ^ x) * x + (J ^ y) * y) & J))
+PAINTER(33, MONO(((J + x) * x + (J + y) * y) & J))
+PAINTER(34, MONO(((J * x) * x + (J * y) * y) & J))
 
 // Class 7
-
-PAINTER(35) {
-  FOR_YX
-    CURRENT_PX = MONO(((J & x) * x + (J & y) * y) % J);
-}
-
-PAINTER(36) {
-  FOR_YX
-    CURRENT_PX = MONO(((J | x) * x + (J | y) * y) % J);
-}
-
-PAINTER(37) {
-  FOR_YX
-    CURRENT_PX = MONO(((J ^ x) * x + (J ^ y) * y) % J);
-}
-
-PAINTER(38) {
-  FOR_YX
-    CURRENT_PX = MONO(((J + x) * x + (J + y) * y) % J);
-}
-
-PAINTER(39) {
-  FOR_YX
-    CURRENT_PX = MONO(((J * x) * x + (J * y) * y) % J);
-}
+PAINTER(35, MONO(((J & x) * x + (J & y) * y) % J))
+PAINTER(36, MONO(((J | x) * x + (J | y) * y) % J))
+PAINTER(37, MONO(((J ^ x) * x + (J ^ y) * y) % J))
+PAINTER(38, MONO(((J + x) * x + (J + y) * y) % J))
+PAINTER(39, MONO(((J * x) * x + (J * y) * y) % J))
 
 // Class 8
-
-PAINTER(40) {
-  FOR_YX
-    CURRENT_PX = RAMP((J & x) * x + (J & y) * y);
-}
-
-PAINTER(41) {
-  FOR_YX
-    CURRENT_PX = RAMP((J | x) * x + (J | y) * y);
-}
-
-PAINTER(42) {
-  FOR_YX
-    CURRENT_PX = RAMP((J ^ x) * x + (J ^ y) * y);
-}
-
-PAINTER(43) {
-  FOR_YX
-    CURRENT_PX = RAMP((J + x) * x + (J + y) * y);
-}
-
-PAINTER(44) {
-  FOR_YX
-    CURRENT_PX = RAMP((J * x) * x + (J * y) * y);
-}
+PAINTER(40, RAMP((J & x) * x + (J & y) * y))
+PAINTER(41, RAMP((J | x) * x + (J | y) * y))
+PAINTER(42, RAMP((J ^ x) * x + (J ^ y) * y))
+PAINTER(43, RAMP((J + x) * x + (J + y) * y))
+PAINTER(44, RAMP((J * x) * x + (J * y) * y))
 
 static void (*hj_painters[])(void) = {
   hj_painter0,  hj_painter1,  hj_painter2,  hj_painter3,  hj_painter4,
@@ -332,8 +152,6 @@ static void (*hj_painters[])(void) = {
 };
 
 #undef PAINTER
-#undef FOR_YX
-#undef CURRENT_PX
 #undef MONO
 #undef RAMP
 #undef J
