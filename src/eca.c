4e48a5f4ab18eb5b2f1d8857165aa7e9cf6237d2
Hussam al-Homsi <sawuare@gmail.com>
2022-04-12 15:21:49 -0400

    142

--- src/eca.c
+++ src/eca.c
@@ -2,18 +2,19 @@
 
 #include "eca.h"
 
-#define RAND_BITS_LESS1 31
+#define RAND_LSB 0
+#define RAND_MSB 31
 
 static uint32_t random = 1;
 
 uint32_t eca_rand(void) {
   uint32_t accumulator = 0;
 
-  for (int b = 0; b <= RAND_BITS_LESS1; ++b)
+  for (int b = RAND_LSB; b <= RAND_MSB; ++b)
     accumulator |= (uint32_t) eca_rule30(
-      (uint32_t) 1 << (b == RAND_BITS_LESS1 ? 0 : b + 1) & random,
+      (uint32_t) 1 << (b == RAND_MSB ? RAND_LSB : b + 1) & random,
       (uint32_t) 1 <<                             b      & random,
-      (uint32_t) 1 << (b == 0 ? RAND_BITS_LESS1 : b - 1) & random) << b;
+      (uint32_t) 1 << (b == RAND_LSB ? RAND_MSB : b - 1) & random) << b;
 
   return random = accumulator;
 }
