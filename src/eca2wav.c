fc7d11658043d619706a65533b6678c09fbbd8f6
Hussam al-Homsi <sawuare@gmail.com>
2022-06-03 05:59:18 -0400

    185

--- src/eca2wav.c
+++ src/eca2wav.c
@@ -12,6 +12,12 @@
 #include "eca.h"
 #include "wafer.h"
 
+#define AMP 127
+#define P2P 254
+#define MAX 255
+#define MID 128
+#define MIN 1
+
 int main(int argc, char *argv[]) {
   uint8_t  rule       = 18;
   uint32_t seed       = 0;
@@ -61,12 +67,11 @@ int main(int argc, char *argv[]) {
     cells[cell_count / 2] = 1;
   }
 
-  _Bool up = 0;
   uint32_t last_cell_pos = cell_count - 1;
 
   for (uint32_t g = 0; g < gen_count; ++g) {
     for (uint32_t c = 0; c < cell_count; ++c) {
-      audio[g * cell_count + c] = cells[c] ? (up = !up) ? 255 : 1 : 128;
+      audio[g * cell_count + c] = cells[c];
 
       accumulators[c] = eca_rule(rule,
         cells[c == 0 ? last_cell_pos : c - 1],
@@ -79,6 +84,76 @@ int main(int argc, char *argv[]) {
 
   free(cells);
   free(accumulators);
+
+  _Bool no_alive_cells = 1;
+  size_t first_alive_cell_pos = 0, last_alive_cell_pos = 0, i = 0, last_sample_pos = sample_count - 1;
+
+  // Search for the first alive cell
+  do {
+    if (audio[i]) {
+      no_alive_cells = 0;
+      first_alive_cell_pos = i;
+      break;
+    }
+  } while (i++ != last_sample_pos);
+
+  // Silence
+  if (no_alive_cells) {
+    i = 0;
+
+    do {
+      audio[i] = MID;
+    } while (i++ != last_sample_pos);
+  }
+  // Linear interpolation
+  else {
+    i = last_sample_pos;
+
+    // Search for the last alive cell
+    do {
+      if (audio[i]) {
+        last_alive_cell_pos = i;
+        break;
+      }
+    } while (i--);
+
+    // First line
+    if (first_alive_cell_pos) {
+      i = 0;
+
+      do {
+        audio[i] = MID + i * AMP / first_alive_cell_pos;
+      } while (++i != first_alive_cell_pos);
+    }
+
+    _Bool up = 0;
+
+    // Mid lines
+    if (first_alive_cell_pos != last_alive_cell_pos) {
+      i = first_alive_cell_pos;
+
+      do {
+        size_t from = i;
+
+        while (!audio[++i]);
+
+        size_t span = i - from;
+
+        for (size_t j = 0; j < span; ++j)
+          audio[from + j] = up ? MIN + j * P2P / span : MAX - j * P2P / span;
+
+        up = !up;
+      } while (i != last_alive_cell_pos);
+    }
+
+    size_t last_few = sample_count - last_alive_cell_pos;
+    i = 0;
+
+    // Last line
+    do {
+      audio[last_alive_cell_pos + i] = up ? MIN + i * AMP / last_few : MAX - i * AMP / last_few;
+    } while (++i != last_few);
+  }
 
   // The longest filename is
   // r100s1000000000c1000000000g1000000000.eca.wav
