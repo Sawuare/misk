d1264092fa60ebd7620e3574aa943be4facc4192
Hussam al-Homsi <sawuare@gmail.com>
2022-08-27 15:36:08 -0400

    234

--- src/hqz.h
+++ src/hqz.h
@@ -5,8 +5,8 @@
 
 #define Q hqz_q
 
-#define PAINTER(id, formula)                                         \
-  static void hqz_painter##id(void) {                                \
+#define P(id, formula)                                               \
+  static void hqz_p##id(void) {                                      \
     uint32_t yn = hqz_y0 + hqz_height;                               \
     uint32_t xn = hqz_x0 + hqz_width;                                \
                                                                      \
@@ -29,65 +29,65 @@ static _Bool hqz_defined(void) {
   return hqz_id < 40 && (hqz_q || !(hqz_id / 5 % 2));
 }
 
-PAINTER(0, (x & y) & Q)
-PAINTER(1, (x | y) & Q)
-PAINTER(2, (x ^ y) & Q)
-PAINTER(3, (x + y) & Q)
-PAINTER(4, (x * y) & Q)
+P(0, (x & y) & Q)
+P(1, (x | y) & Q)
+P(2, (x ^ y) & Q)
+P(3, (x + y) & Q)
+P(4, (x * y) & Q)
 
-PAINTER(5, (x & y) % Q)
-PAINTER(6, (x | y) % Q)
-PAINTER(7, (x ^ y) % Q)
-PAINTER(8, (x + y) % Q)
-PAINTER(9, (x * y) % Q)
+P(5, (x & y) % Q)
+P(6, (x | y) % Q)
+P(7, (x ^ y) % Q)
+P(8, (x + y) % Q)
+P(9, (x * y) % Q)
 
-PAINTER(10, (x * x & y * y) & Q)
-PAINTER(11, (x * x | y * y) & Q)
-PAINTER(12, (x * x ^ y * y) & Q)
-PAINTER(13, (x * x + y * y) & Q)
-PAINTER(14, (x * x * y * y) & Q)
+P(10, (x * x & y * y) & Q)
+P(11, (x * x | y * y) & Q)
+P(12, (x * x ^ y * y) & Q)
+P(13, (x * x + y * y) & Q)
+P(14, (x * x * y * y) & Q)
 
-PAINTER(15, (x * x & y * y) % Q)
-PAINTER(16, (x * x | y * y) % Q)
-PAINTER(17, (x * x ^ y * y) % Q)
-PAINTER(18, (x * x + y * y) % Q)
-PAINTER(19, (x * x * y * y) % Q)
+P(15, (x * x & y * y) % Q)
+P(16, (x * x | y * y) % Q)
+P(17, (x * x ^ y * y) % Q)
+P(18, (x * x + y * y) % Q)
+P(19, (x * x * y * y) % Q)
 
-PAINTER(20, ((Q & x) * x & (Q & y) * y) & Q)
-PAINTER(21, ((Q | x) * x & (Q | y) * y) & Q)
-PAINTER(22, ((Q ^ x) * x & (Q ^ y) * y) & Q)
-PAINTER(23, ((Q + x) * x & (Q + y) * y) & Q)
-PAINTER(24, ((Q * x) * x & (Q * y) * y) & Q)
+P(20, ((Q & x) * x & (Q & y) * y) & Q)
+P(21, ((Q | x) * x & (Q | y) * y) & Q)
+P(22, ((Q ^ x) * x & (Q ^ y) * y) & Q)
+P(23, ((Q + x) * x & (Q + y) * y) & Q)
+P(24, ((Q * x) * x & (Q * y) * y) & Q)
 
-PAINTER(25, ((Q & x) * x & (Q & y) * y) % Q)
-PAINTER(26, ((Q | x) * x & (Q | y) * y) % Q)
-PAINTER(27, ((Q ^ x) * x & (Q ^ y) * y) % Q)
-PAINTER(28, ((Q + x) * x & (Q + y) * y) % Q)
-PAINTER(29, ((Q * x) * x & (Q * y) * y) % Q)
+P(25, ((Q & x) * x & (Q & y) * y) % Q)
+P(26, ((Q | x) * x & (Q | y) * y) % Q)
+P(27, ((Q ^ x) * x & (Q ^ y) * y) % Q)
+P(28, ((Q + x) * x & (Q + y) * y) % Q)
+P(29, ((Q * x) * x & (Q * y) * y) % Q)
 
-PAINTER(30, ((Q & x) * x + (Q & y) * y) & Q)
-PAINTER(31, ((Q | x) * x + (Q | y) * y) & Q)
-PAINTER(32, ((Q ^ x) * x + (Q ^ y) * y) & Q)
-PAINTER(33, ((Q + x) * x + (Q + y) * y) & Q)
-PAINTER(34, ((Q * x) * x + (Q * y) * y) & Q)
+P(30, ((Q & x) * x + (Q & y) * y) & Q)
+P(31, ((Q | x) * x + (Q | y) * y) & Q)
+P(32, ((Q ^ x) * x + (Q ^ y) * y) & Q)
+P(33, ((Q + x) * x + (Q + y) * y) & Q)
+P(34, ((Q * x) * x + (Q * y) * y) & Q)
 
-PAINTER(35, ((Q & x) * x + (Q & y) * y) % Q)
-PAINTER(36, ((Q | x) * x + (Q | y) * y) % Q)
-PAINTER(37, ((Q ^ x) * x + (Q ^ y) * y) % Q)
-PAINTER(38, ((Q + x) * x + (Q + y) * y) % Q)
-PAINTER(39, ((Q * x) * x + (Q * y) * y) % Q)
+P(35, ((Q & x) * x + (Q & y) * y) % Q)
+P(36, ((Q | x) * x + (Q | y) * y) % Q)
+P(37, ((Q ^ x) * x + (Q ^ y) * y) % Q)
+P(38, ((Q + x) * x + (Q + y) * y) % Q)
+P(39, ((Q * x) * x + (Q * y) * y) % Q)
 
 static void (*hqz_painters[40])(void) = {
-  // AND         OR             XOR            ADD            MUL
-  hqz_painter0,  hqz_painter1,  hqz_painter2,  hqz_painter3,  hqz_painter4,  // Class 0
-  hqz_painter5,  hqz_painter6,  hqz_painter7,  hqz_painter8,  hqz_painter9,  // Class 1
-  hqz_painter10, hqz_painter11, hqz_painter12, hqz_painter13, hqz_painter14, // Class 2
-  hqz_painter15, hqz_painter16, hqz_painter17, hqz_painter18, hqz_painter19, // Class 3
-  hqz_painter20, hqz_painter21, hqz_painter22, hqz_painter23, hqz_painter24, // Class 4
-  hqz_painter25, hqz_painter26, hqz_painter27, hqz_painter28, hqz_painter29, // Class 5
-  hqz_painter30, hqz_painter31, hqz_painter32, hqz_painter33, hqz_painter34, // Class 6
-  hqz_painter35, hqz_painter36, hqz_painter37, hqz_painter38, hqz_painter39  // Class 7
+  // AND   OR       XOR      ADD      MUL
+  hqz_p0,  hqz_p1,  hqz_p2,  hqz_p3,  hqz_p4,  // Class 0
+  hqz_p5,  hqz_p6,  hqz_p7,  hqz_p8,  hqz_p9,  // Class 1
+  hqz_p10, hqz_p11, hqz_p12, hqz_p13, hqz_p14, // Class 2
+  hqz_p15, hqz_p16, hqz_p17, hqz_p18, hqz_p19, // Class 3
+  hqz_p20, hqz_p21, hqz_p22, hqz_p23, hqz_p24, // Class 4
+  hqz_p25, hqz_p26, hqz_p27, hqz_p28, hqz_p29, // Class 5
+  hqz_p30, hqz_p31, hqz_p32, hqz_p33, hqz_p34, // Class 6
+  hqz_p35, hqz_p36, hqz_p37, hqz_p38, hqz_p39  // Class 7
 };
 
 #undef Q
-#undef PAINTER
+#undef P
